8d1a2ee21a40a8053fcd656b3ec6d93d1108e22a
AntoniDimes <duyanhno838@gmail.com>
2025-02-01 21:33:13 -0800

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,18 +1,12 @@
 var userInput = prompt("Enter a number:");
-  
-if (userInput === null || userInput.trim() === "" || isNaN(userInput)) 
-{
-  userInput = 0;
-} 
-else 
-{
-  userInput = parseInt(userInput);
+
+if (userInput === null || userInput.trim() === "" || isNaN(userInput)) {
+    userInput = 0;
+} else {
+    userInput = parseInt(userInput);
 }
 
-var table = userInput;         // Unit of table
-
-// operator = GetValueFromUser("operator");
-// table = GetValueFromUser("table")
+var table = userInput; // Unit of table
 
 var msg = '<h2>Multiplication Table</h2>';
 
@@ -20,25 +14,13 @@ var msg = '<h2>Multiplication Table</h2>';
 var el = document.getElementById('blackboard');
 el.innerHTML = msg + GetTableContent(table);
 
-/* function GetValueFromUser(valueType) {
-  var greetingMessage = 'Hello. How are you?';
-  if (valueType == "operator") {
-    greetingMessage += "Enter addition or multiplication";
-  }
-  else
-  {
-    greetingMessage += "Enter table";
-  }
-  return prompt(greetingMessage);
-} */
-
 function GetTableContent(table) {
-  var i = 1;                 // Set counter to 1
-  var msg = '';              // Message  
-  // Do multiplication
-  while (i < 11) {
-      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-      i++;
-  }
-  return msg;
+    var i = 1; // Set counter to 1
+    var msg = ''; // Message  
+    // Do multiplication
+    while (i < 11) {
+        msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+        i++;
+    }
+    return msg;
 }
