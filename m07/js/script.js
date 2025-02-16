3e227e6ac7d06ce45e127167e123b161c74a40d0
AntoniDimes <duyanhno838@gmail.com>
2025-02-15 20:48:47 -0800

    Update script.js

--- m07/js/script.js
+++ m07/js/script.js
@@ -1,1 +1,17 @@
+// Create the template for objects that are hotels
+function Hotel(name, rooms, booked) {
+  this.name = name;
+  this.rooms = rooms;
+  this.booked = booked;
+  this.checkAvailability = function() {
+    return this.rooms - this.booked;
+  };
+}
 
+function Display() {
+  var hotel = new Hotel("Sunset", 86, 10);
+  var available_room = hotel.checkAvailability();
+  document.getElementById("result").innerHTML = hotel.name + " rooms: " + available_room;
+}
+
+Display();
