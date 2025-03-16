0c99042a3429acb4ebc2e571d8870d87bdd2bbd8
AntoniDimes <duyanhno838@gmail.com>
2025-03-15 20:02:41 -0700

    Update play_music.js

--- finalp1/js/play_music.js
+++ finalp1/js/play_music.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("click", function() {
     let audio = document.getElementById("bg-music");
     audio.volume = 0.2; // Set volume to 20%
     audio.play();
